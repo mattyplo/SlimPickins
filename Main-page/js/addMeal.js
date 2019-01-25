83a53d623d98bce6a44e539fe07f03c3d6c83213
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-01-24 17:51:30 -0800

    added js file and a simple button in the html that responds to a click event

---
+++ Main-page/js/addMeal.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+ 
+  
+  var addMealButton = document.getElementById("addMeal"); 
+  
+  addMealButton.addEventListener("click", function() {
+    alert("hey");l
+  });
+  
+});
