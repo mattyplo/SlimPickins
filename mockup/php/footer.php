fa34ca8c3d3f94633e78d5e2312d70cf770230a1
YunFSui <syatwork@outlook.com>
2019-01-25 20:26:16 -0800

    initial commit on mock up, journal page UI still the same, change to reusable header/footer

---
+++ mockup/php/footer.php
@@ -0,0 +1,31 @@
+<<!DOCTYPE html>
+<html>
+<body>
+    <table>
+        <tr>
+        <th>Our Products</th>
+            <th>More Info</th>
+            <th>Connect</th>
+        </tr>
+        <tr>
+            <td><a href="order.html">Order</a></td>
+            <td><a href="about.html">Our Story</a></td>
+            <td><a  href="contact.html">Contact</a></td>
+        </tr>
+        <tr>
+            <td><a href="gallery.html">Gallery</a></td>
+            <td><a href="faq.html">FAQ</a></td>
+            <td><a href="order.html">Order</a></td>
+        </tr>
+        
+        
+        
+    </table>
+    <ul>
+        <li>Copyright 2018 &copy;</li>
+        <li>All Rights Reserved</li>
+    </ul>
+
+    <p class = "valid"><a href = "https://validator.w3.org/check?uri=referer">Valid</a></p>
+</body>
+</html>
---
+++ mockup/php/header.php
@@ -0,0 +1,21 @@
+<div id = "header">
+
+    <a class="logo" href = "index.html"><h1 class="logo">Slim Pickins</h1></a>
+        
+        
+    <nav id= "primary">
+        <ul>
+            <li><a href="index.html">Home</a>
+            </li>
+            <li><a href="profile.html">Profile</a>
+            </li>
+            <li><a href="foodlist.html">Search</a>
+            </li>
+            
+            <li><a href="faq.html">FAQ</a>
+            </li>
+            <li><a href = "landing.html">Login</a></li>
+        </ul>
+    </nav>
+
+</div>
