68995206a3dd553d41ff34693266bacf2594c7ec
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-01-26 19:42:49 -0800

    fixed a typo in the alert on the addMeal.js file

--- Main-page/js/addMeal.js
+++ Main-page/js/addMeal.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var addMealButton = document.getElementById("addMeal"); 
   
   addMealButton.addEventListener("click", function() {
-    alert("hey");l
+    alert("hey");
   });
   
 });
