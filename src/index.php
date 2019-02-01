38de0fb8068190af06733260e7ac0e77da740924
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-01-31 17:10:25 -0800

    add html for new food item in form on journal page

--- src/index.php
+++ src/index.php
@@ -47,6 +47,17 @@
             <br>
             <br>
             <input type="radio" name="newOrExisting" value="new" checked> New <br>
+            
+            <div id="new">
+              <br>
+              <label for="foodItem"><b>Food Name</b></label>
+              <input type="text" name="foodName">
+              <label for="gramsPerCalorie"><b>Grams Per Calorie</b></label>
+              <input type="number" name="gramsPerCalorie">
+              <label for="gramsPerServing"><b>Grams Per Serving</b></label>
+              <input type="number" name="gramsPerServing">
+            </div>
+            
             <input type="radio" name="newOrExisting" value="existing"> Existing <br>
             
             <div id="existing">
