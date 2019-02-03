d4853681bb18476e6330278a275cdc96a64677ed
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-02-02 21:52:15 -0800

    fixed FoodsInsert.sql so that the ID is no longer in the Values, because it should be a auto increment value

--- DataModel/FoodsInsert.sql
+++ DataModel/FoodsInsert.sql
@@ -1,1 +1,1 @@
-INSERT INTO `Foods` VALUES (3,'Pizza',3.00,107.00),(4,'Kale',2.03,67.00),(12,'Bacon',3.85,26.00),(13,'Yoghurt',1.00,245.00),(14,'Carrot',2.44,61.00);
+INSERT INTO `Foods` VALUES ('Pizza',3.00,107.00),('Kale',2.03,67.00),('Bacon',3.85,26.00),('Yoghurt',1.00,245.00),('Carrot',2.44,61.00);
