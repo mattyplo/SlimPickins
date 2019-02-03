84cb7179eb1d2de9e6700389100217e46fe823f1
[choisojung] <[sojc31@gmail.com]>
2019-02-03 13:05:50 -0800

    added functionality to add new user to database

---
+++ src/php/createNewUser.php
@@ -0,0 +1,31 @@
+<?php
+
+require("dbconnection.php");
+
+if($_SERVER['REQUEST_METHOD']==='POST') {
+    
+    $sql ="INSERT INTO Users ";
+    $sql.="(FirstName, LastName, UserName, Password) ";
+    $sql.="VALUES (";
+    $sql.="'" . $_POST['firstName']."',";
+    $sql.="'" . $_POST['lastName'] ."',";
+    $sql.="'" . $_POST['userName'] ."',";
+    $sql.="'" . $_POST['password'] ."'";
+    $sql.=")";
+   $result = mysqli_query($conn, $sql);
+    
+    
+    if ($result){
+        die("die");
+    }else {
+        die(mysqli_error($conn));
+        echo mysqli_error($conn);
+        db_disconnect($conn);
+        exit;
+    }    
+}
+
+header("Location:../index.php");
+
+
+?>
