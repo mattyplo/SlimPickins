5469ce44a16ce9a69425f0658e404e589e32d177
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-02-10 11:35:43 -0800

    journalModel.php now sorts the userMeals by date

--- src/Model/journalModel.php
+++ src/Model/journalModel.php
@@ -4,27 +4,23 @@
   $sql = "SELECT * FROM Meals ";
   $sql .= "WHERE UserID = 2";
   $userMeals = mysqli_query($conn, $sql);
-  
-
+ 
+  // Array to store meals into
   $meals = [];
+  
+  // This function will sort the meals array by date
+  function date_compare($a, $b) {
+      $t1 = strtotime($a['date']);
+      $t2 = strtotime($b['date']);
+      return $t1 - $t2;
+  }    
+  
   foreach ($userMeals as $meal=>$val) {
     $meals[$meal] = $val['MealID'];
     $meals[$meal] = [];
     $meals[$meal]['date'] = $val['Date'];
     $meals[$meal]['mealType'] = $val['MealTypeID'];
-    
-   /* foreach ($val as $meal) {
-      $mealID = $meal['MealID'];
-      $meals[$mealID]['date'] = $meal['Date'];
-      $meals[$mealID]['mealType'] = $meal['MealTypeID'];
-    }*/
   }
-  //$sql = "SELECT * FROM MealsFoods";
-  //$sql = "WHERE"
 
-
-
-
-
-
+  usort($meals, 'date_compare');
 ?>
