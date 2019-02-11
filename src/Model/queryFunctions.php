baad1b9db3777bf0805cd093a8946131f3d265ba
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-02-11 14:45:36 -0800

    refactored selectMeal and selectFoodID into queryFunctions.php

--- src/Model/queryFunctions.php
+++ src/Model/queryFunctions.php
@@ -33,6 +33,8 @@ function insertMeal($meal) {
  
 }
 
+/***************** MealsFoods Queries *********************/
+
 function insertMealsFoods($data) {
   
   global $conn;
@@ -46,6 +48,18 @@ function insertMealsFoods($data) {
   mysqli_query($conn, $sql);
 }
 
+function selectFoodID($mealID) {
+  // Currently only returns One, but multiple foods may be stored eventually
+  global $conn;
+  
+  $sql = "SELECT FoodID, GramsConsumed FROM mealsfoods
+WHERE MEALID = ";
+  $sql .= $mealID;
+  $mealFoodsResult = mysqli_query($conn, $sql);
+  $mealFoods = mysqli_fetch_row($mealFoodsResult);
+  return (int)$mealFoods[0];
+  
+}
 function insertUser($user) {
   
   global $conn;
@@ -73,6 +87,15 @@ function selectUser($user) {
   $sql .= $user['password'] . "';";
   return mysqli_query($conn, $sql);
 
+}
+
+function selectMeals($userID) {
+  
+  global $conn;
+  
+  $sql = "SELECT * FROM Meals ";
+  $sql .= "WHERE UserID = " . $userID;
+  return mysqli_query($conn, $sql);
 }
 
 ?>
